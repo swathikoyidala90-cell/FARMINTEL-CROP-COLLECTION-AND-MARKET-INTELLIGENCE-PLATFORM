5d0c35f6fef91a23e0751d9f15c305803e79963e
swathikoyidala90 <swathikoyidala90@gmail.com>
2026-06-06 19:49:28 +0530

    Cleaned up nested repo

--- farmintel-frontend/src/api.js
+++ farmintel-frontend/src/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 import { API_BASE_URL } from "./config";
 
 const API = axios.create({
-  baseURL: `${API_BASE_URL}/api`
+  baseURL: `${API_BASE_URL}/api`,
+  FARMER: import.meta.env.VITE_FARMER_API_URL,
+  CUSTOMER: import.meta.env.VITE_CUSTOMER_API_URL,
+  STAFF: import.meta.env.VITE_STAFF_API_URL,
+  ADMIN: import.meta.env.VITE_ADMIN_API_URL,
 });
 
 export const getBestMarket = (crop, location) => {
